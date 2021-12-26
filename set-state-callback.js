7d36def82f9ffef94804db914bbc109f44fa62d7
vipulpathak113 <vipulpathak113@gmail.com>
2021-12-26 23:48:24 +0530

    set-state-callback npm package

---
+++ set-state-callback.js
@@ -0,0 +1,25 @@
+import { useState, useEffect,useRef } from 'react';
+
+const useStateCallback = initialValue => {
+  const callbackRef = useRef(null);
+
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    if (callbackRef.current) {
+      callbackRef.current(value);
+
+      callbackRef.current = null;
+    }
+  }, [value]);
+
+  const setValueWithCallback = (newValue, callback) => {
+    callbackRef.current = callback;
+
+    return setValue(newValue);
+  };
+
+  return [value, setValueWithCallback];
+};
+
+export default useStateCallback;
